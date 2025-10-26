b9cee5824ad8975ecd7f872769926b9d58dc8554
Oguzhan <o.ayg@protonmail.com>
2025-10-26 17:57:04 +0100

    Material-Klasse hinzugefügt für simpleren Zugriff mit GUI

--- material.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-class Material:
-    def __init__ (self, material, dicke, brechzahl):
-        self.material = material
-        self.dicke = dicke
-        self.brechzahl = brechzahl
